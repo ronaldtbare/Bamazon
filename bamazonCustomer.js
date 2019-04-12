35b1e1171924ccae81c1b7b1cf9992a56671216b
travisbare <travisbare@gmail.com>
2019-04-12 12:33:19 -0500

    initial files, inquirer

---
+++ bamazonCustomer.js
@@ -0,0 +1,202 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+//global variables
+var id = 0;
+var quantity = 0;
+
+// function which prompts the user for what action they should take
+function start() {
+  inquirer
+    .prompt([
+      {
+      name: "selectedid",
+      type: "input",
+      message: "What is the ID of the product you would like to buy?" 
+    },
+
+    {
+      name: "selectedquantity",
+      type: "input",
+      message: "What is the purchase quantity of this item?"
+    }
+  ])
+    .then(function(answers) {
+     
+      console.log(JSON.stringify(answers));
+      console.log("quantity: ",answers.selectedquantity);
+      console.log("ID: ",answers.selectedid);
+      var id = answers.selectedid;
+      var quantity = answers.selectedquantity;
+      //update database
+      update(id,quantity);
+     }
+      
+);
+
+
+function update(id,quantity){
+  console.log("the update id is: ",id);
+  console.log("the update quantity is: ",quantity);
+
+  connection.query("SELECT stock_quantity FROM products WHERE item_id = ?",id,
+  function(err,rows){
+    if(err){
+      console.log(err);
+      return;
+    }
+    rows.forEach(function(result){
+
+      var db_quantity = result.stock_quantity;
+      console.log("The quantity in the database is ",db_quantity);
+
+      if (db_quantity<quantity){
+        console.log("There is not enough stock to fill this order.");
+        return;
+      }
+      
+      if(db_quantity>=quantity){
+        console.log("we will update db quantity.")
+      }
+     
+    })
+
+    
+
+  });
+}
+// connection.end();
+
+    }
+
+// // function to handle posting new items up for auction
+// function postAuction() {
+//   // prompt for info about the item being put up for auction
+//   inquirer
+//     .prompt([
+//       {
+//         name: "item",
+//         type: "input",
+//         message: "What is the item you would like to submit?"
+//       },
+//       {
+//         name: "category",
+//         type: "input",
+//         message: "What category would you like to place your auction in?"
+//       },
+//       {
+//         name: "startingBid",
+//         type: "input",
+//         message: "What would you like your starting bid to be?",
+//         validate: function(value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           return false;
+//         }
+//       }
+//     ])
+//     .then(function(answer) {
+//       // when finished prompting, insert a new item into the db with that info
+//       connection.query(
+//         "INSERT INTO auctions SET ?",
+//         {
+//           item_name: answer.item,
+//           category: answer.category,
+//           starting_bid: answer.startingBid || 0,
+//           highest_bid: answer.startingBid || 0
+//         },
+//         function(err) {
+//           if (err) throw err;
+//           console.log("Your auction was created successfully!");
+//           // re-prompt the user for if they want to bid or post
+//           start();
+//         }
+//       );
+//     });
+// }
+
+// function bidAuction() {
+//   // query the database for all items being auctioned
+//   connection.query("SELECT * FROM auctions", function(err, results) {
+//     if (err) throw err;
+//     // once you have the items, prompt the user for which they'd like to bid on
+//     inquirer
+//       .prompt([
+//         {
+//           name: "choice",
+//           type: "rawlist",
+//           choices: function() {
+//             var choiceArray = [];
+//             for (var i = 0; i < results.length; i++) {
+//               choiceArray.push(results[i].item_name);
+//             }
+//             return choiceArray;
+//           },
+//           message: "What auction would you like to place a bid in?"
+//         },
+//         {
+//           name: "bid",
+//           type: "input",
+//           message: "How much would you like to bid?"
+//         }
+//       ])
+//       .then(function(answer) {
+//         // get the information of the chosen item
+//         var chosenItem;
+//         for (var i = 0; i < results.length; i++) {
+//           if (results[i].item_name === answer.choice) {
+//             chosenItem = results[i];
+//           }
+//         }
+
+//         // determine if bid was high enough
+//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
+//           // bid was high enough, so update db, let the user know, and start over
+//           connection.query(
+//             "UPDATE auctions SET ? WHERE ?",
+//             [
+//               {
+//                 highest_bid: answer.bid
+//               },
+//               {
+//                 id: chosenItem.id
+//               }
+//             ],
+//             function(error) {
+//               if (error) throw err;
+//               console.log("Bid placed successfully!");
+//               start();
+//             }
+//           );
+//         }
+//         else {
+//           // bid wasn't high enough, so apologize and start over
+//           console.log("Your bid was too low. Try again...");
+//           start();
+//         }
+//       });
+//   });
+// }
